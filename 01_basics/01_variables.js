35fc5529d989c12dc0d6283caf25b9d1d9b6b8ad
rajkrishna1432 <00rajkrishna99@gmail.com>
2025-01-29 21:21:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 233300
+let accountEmail = "krishna@google.com"
+var accountPassword =12345
+accountCity ="Vadodara"
+let accountState;
+// accountId =2 // not allowed
+accountEmail="raj@123"
+accountPassword="9956"
+accountCity="Ghazipur"
+console.log(accountId);
+/*
+    prefer not to use var
+    because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use stirct";
+// alert (3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 3) 
+
+console.log("rajbhar"); //we use semicolon it depend on us
+
+let name="krishna"
+let age= 20
+let isloggedIn=false
+
+// number => 2 to pwer 53
+// bigInt 
+// string =>" "
+// boolean =>true/false
+// null => standalone value
+// undefined=> "means the value is not assigned"
+//symbol=>unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+
