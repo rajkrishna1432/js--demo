8a2f1080cd24a864bcfc4d215a1b0ac7eff5f805
rajkrishna1432 <00rajkrishna99@gmail.com>
2025-01-30 07:31:12 +0000

    comparison of code

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>=1);
+
+// console.log("2" > 1); //this will ok,but comparison alway should be same datatypes
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+
+// console.log(undefined > 0); // in undefined all comparison is always false
+// console.log(undefined == 0);
+
+// === strictly check
+
+console.log("2"===2); //this is not equal to 2,because they have different datatype
+
+
+
+
+
+
