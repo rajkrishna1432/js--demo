fb21855e0884699c0e9ee03ab9e793e3f4a12b49
rajkrishna1432 <00rajkrishna99@gmail.com>
2025-01-30 07:00:38 +0000

    string conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,4 +25,36 @@ let stringNumber =String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//*******************operations***************//
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2**3); //double star is used for power
+
+let str1="krishan"
+let str2=" rajbhar"
+let str3= str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log((3 + 4) * 5 % 6);
+
+console.log(+true); //this is not good method,but the value is outcome 1
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+// link to conversion
+// https://tc39.es/ecma262/#sec-abstract-operations
+
+
+
+
 
